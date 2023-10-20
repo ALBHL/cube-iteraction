824de4cf98f06549dfe59e2a622b904c4952fdcf
Apple <>
2023-10-20 19:19:08 +0000

    Initial commit.

---
+++ SceneReconstructionExample/SceneReconstructionExampleApp.swift
@@ -0,0 +1,33 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The app structure.
+*/
+
+import OSLog
+import SwiftUI
+
+/// The entry point for the app.
+@main
+@MainActor
+struct SceneReconstructionExampleApp: App {
+    @State private var model = EntityModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+        WindowGroup(id: "error") {
+            Text("An error occurred; check the app's logs for details.")
+        }
+
+        ImmersiveSpace(id: cubeMeshInteractionID) {
+            CubeMeshInteraction()
+                .environment(model)
+        }
+    }
+}
+
+let logger = Logger(subsystem: "com.apple-samplecode.SceneReconstructionExample", category: "general")
